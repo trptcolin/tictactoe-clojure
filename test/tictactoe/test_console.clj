758fbfaad5bf1075bb8a71fe852848467ea5b142
Colin Jones <trptcolin@gmail.com>
2010-04-08 09:18:34 -0500

    using builtin stub for *in*

--- test/tictactoe/test_console.clj
+++ test/tictactoe/test_console.clj
@@ -5,24 +5,20 @@
     [tictactoe.core :only [make-board final-message]]))
 
 (defn fake-println [& args])
-(def ints-to-be-read (ref '()))
-(defn fake-read-int []
-  (let [i (first @ints-to-be-read)]
-    (dosync
-      (alter ints-to-be-read rest))
-    i))
 
 (use-fixtures :each
   (fn [f] (binding [user-message fake-println
-                    user-prompt fake-println
-                    read-int fake-read-int] (f))))
+                    user-prompt fake-println] (f))))
+
+(defn make-input [coll]
+  (apply str (interleave coll (repeat "\n"))))
 
 (deftest test-human-mover
-  (binding [ints-to-be-read (ref (iterate inc 1))]
+  (with-in-str (make-input '(1 2 3))
     (is (= 0 (human-mover (make-board) "X")))
     (is (= 1 (human-mover (make-board) "X")))
     (is (= 2 (human-mover (make-board) "X"))))
-  (binding [ints-to-be-read (ref (filter even? (reverse (range 9))))]
+  (with-in-str (make-input '(8 6 4))
     (is (= 7 (human-mover (make-board) "X")))
     (is (= 5 (human-mover (make-board) "X")))
     (is (= 3 (human-mover (make-board) "X")))))
@@ -32,11 +28,9 @@
   (dosync (ref-set actual-winner winner)))
 
 (deftest full-game
-  (binding [final-message mock-final-message
-            ints-to-be-read
-            (ref
-              '(1 ; game type
-                1 9 2 8 3 ; moves
-                ))]
-    (console-play)
-    (is (= "X" @actual-winner))))
+  (binding [final-message mock-final-message]
+    (with-in-str (make-input '(1 1 9 2 8 3))
+      (console-play)
+      (is (= "X" @actual-winner)))))
+
+(run-tests)
